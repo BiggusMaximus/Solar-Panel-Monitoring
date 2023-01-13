9eef74c03e5b8f2c3da52cebada17c51fc1c4047
faizdaffa <faizdafa15@gmail.com>
2023-01-13 20:53:45 +0700

    Tes PCB

---
+++ Arduino-Bluetooth/raspberry/send_to_arduino.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO      
+import os, time
+ 
+GPIO.setmode(GPIO.BOARD)    
+port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=0.5)
+ 
+while True:
+    port.write('A')
+    rcv = port.read(10)
+    print rcv
