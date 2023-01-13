138e3bc285feeee59e0fbf6d7761228648a64903
BiggusMaximus <annastyabagas164@gmail.com>
2023-01-13 22:26:56 +0700

    finishing web

--- Arduino-Bluetooth/raspberry/send_to_arduino.py
+++ Arduino-Bluetooth/raspberry/send_to_arduino.py
@@ -1,6 +1,7 @@
 import serial
+import time
 
-serial_arduino = serial.Serial('/dev/ttyUSB0',9600)
+serial_arduino = serial.Serial('/dev/ttyUSB0', 9600)
 
 while True:
     if serial_arduino.isOpen():
@@ -12,11 +13,12 @@ while True:
             if not not temp.decode():
                 val = (val.decode()+temp.decode()).encode()
         val = val.decode().rstrip().split(',')
-        
+
         voltage_generated = val[0]
         voltage_used = val[1]
         current_generated = val[2]
         current_used = val[3]
         power_used = val[4]
         power_generated = val[5]
-        print(f"voltage generate : {val[0]}  \n current generate : {val[1]} \n piower generate : {val[2]} \n voltage used : {val[3]}  \n current used : {val[4]} \n piower used : {val[5]} \n")
+        print(
+            f"voltage generate : {val[0]}  \n current generate : {val[1]} \n piower generate : {val[2]} \n voltage used : {val[3]}  \n current used : {val[4]} \n piower used : {val[5]} \n")
--- Arduino-Bluetooth/web/App/data.py
+++ Arduino-Bluetooth/web/App/data.py
@@ -1,64 +1,75 @@
 import numpy as np
-#from .timestamp import *
+from .timestamp import *
 import pandas as pd
 import serial
 import time
 
 data = []
-ser = serial.Serial('COM3', 9600)  # ganti 'COM3' dengan nama port serial yang sesuai
+serial_arduino = serial.Serial('/dev/ttyUSB0', 9600)
+
 
 def parsing_data():
-    if ser.isOpen():
-        data = ""
-        while data != None:
-            print('a')
-            ser.write("Send Data".encode('utf-8'))
-            data = ser.readline().strip().decode("utf-8")
-            print(data)
+    if serial_arduino.isOpen():
+        serial_arduino.flushInput()
+        val = serial_arduino.readline()
+        while not '\\n' in str(val):
+            time.sleep(.001)
+            temp = serial_arduino.readline()
+            if not not temp.decode():
+                val = (val.decode()+temp.decode()).encode()
+        val = val.decode().rstrip().split(',')
+        return val
+
 
 def get_data():
     df = {
-    'Voltage_generated': [], 
-        'Current_generated': [], 
+        'Voltage_generated': [],
+        'Current_generated': [],
         'Power_generated': [],
-        'Timestamp_generated' : [],
-        'Voltage_used': [], 
-        'Current_used': [], 
+        'Timestamp_generated': [],
+        'Voltage_used': [],
+        'Current_used': [],
         'Power_used': [],
-        'Timestamp_used' : [] 
+        'Timestamp_used': []
     }
 
-    voltage = np.random.rand()
-    current = np.random.rand()
-    power = np.random.rand()
+    val = parsing_data()
+    voltage_generated = val[0]
+    voltage_used = val[1]
+    current_generated = val[2]
+    current_used = val[3]
+    power_used = val[4]
+    power_generated = val[5]
+
     data.append(
         {
-            'Voltage_generated': str(voltage), 
-            'Current_generated': str(current), 
-            'Power_generated': str(power),
-            'Timestamp_generated' : get_time(),
-            
-            'Voltage_used': str(voltage), 
-            'Current_used': str(current), 
-            'Power_used': str(power),
-            'Timestamp_used' : get_time()
+            'Voltage_generated': str(voltage_generated),
+            'Current_generated': str(current_generated),
+            'Power_generated': str(power_generated),
+            'Timestamp_generated': get_time(),
+
+            'Voltage_used': str(voltage_used),
+            'Current_used': str(current_used),
+            'Power_used': str(power_used),
+            'Timestamp_used': get_time()
         })
-    
-    df['Voltage_generated'].append(str(voltage))
-    df['Current_generated'].append(str(voltage))
-    df['Power_generated'].append(str(voltage))
+
+    df['Voltage_generated'].append(str(voltage_generated))
+    df['Current_generated'].append(str(current_generated))
+    df['Power_generated'].append(str(power_generated))
     df['Timestamp_generated'].append(get_time())
 
-    df['Voltage_used'].append(str(voltage))
-    df['Current_used'].append(str(voltage))
-    df['Power_used'].append(str(voltage))
+    df['Voltage_used'].append(str(voltage_used))
+    df['Current_used'].append(str(current_used))
+    df['Power_used'].append(str(power_used))
     df['Timestamp_used'].append(get_time())
 
     df = pd.DataFrame(df)
     df.to_csv('data.csv', index=False)
     df.to_excel('data.xlsx', index=False)
-    
+
     return data
+
 
 while True:
     parsing_data()
