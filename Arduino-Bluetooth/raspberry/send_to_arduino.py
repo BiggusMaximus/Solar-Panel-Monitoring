e082b107fe046a4264094edc8ec13aef9cb3ee11
BiggusMaximus <jahekeraton164@gmail.com>
2023-01-13 22:22:29 +0700

    finishing arduino raspi serial communication and parsing

--- Arduino-Bluetooth/raspberry/send_to_arduino.py
+++ Arduino-Bluetooth/raspberry/send_to_arduino.py
@@ -1,9 +1,22 @@
 import serial
 
-ser = serial.Serial('/dev/ttyUSB0',9600)
-s = [0]
+serial_arduino = serial.Serial('/dev/ttyUSB0',9600)
+
 while True:
-    read_serial = ser.readline()
-    s[0] = str(int(ser.readline(),16))
-    print(s[0])
-    print(read_serial)
+    if serial_arduino.isOpen():
+        serial_arduino.flushInput()
+        val = serial_arduino.readline()
+        while not '\\n' in str(val):
+            time.sleep(.001)
+            temp = serial_arduino.readline()
+            if not not temp.decode():
+                val = (val.decode()+temp.decode()).encode()
+        val = val.decode().rstrip().split(',')
+        
+        voltage_generated = val[0]
+        voltage_used = val[1]
+        current_generated = val[2]
+        current_used = val[3]
+        power_used = val[4]
+        power_generated = val[5]
+        print(f"voltage generate : {val[0]}  \n current generate : {val[1]} \n piower generate : {val[2]} \n voltage used : {val[3]}  \n current used : {val[4]} \n piower used : {val[5]} \n")
